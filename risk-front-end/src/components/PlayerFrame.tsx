94ce933e88968f7974dc8776f725e5299d0b194d
Alexander Wuyts <alexander.wuyts@student.kdg.be>
2022-11-29 11:49:19 +0100

    Started playerUI

--- risk-front-end/src/components/PlayerFrame.tsx
+++ risk-front-end/src/components/PlayerFrame.tsx
@@ -3,8 +3,7 @@ import Avatar from "@mui/material/Avatar";
 
 export default function PlayerFrame() {
     return (
-        <Box sx={{height:"7vw", mt:"1rem", mr:"1rem", border: 1, borderRadius: 3,
-            alignContent: "center", alignItems: "center", backgroundColor: "#7c7c7d"}}>
+        <Box sx={{height:"7vw", mt:"1rem", mr:"1rem", border: 1, borderRadius: 3, alignContent: "center", alignItems: "center"}}>
             <Avatar sx={{ width: "7vw", height: "7vw" }} src="testAvatar.jpg"></Avatar>
         </Box>
     );
