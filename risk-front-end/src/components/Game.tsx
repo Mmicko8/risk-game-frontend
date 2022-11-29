5a44cad9b002174476c0bf65a34d95772285d028
Alexander Wuyts <alexander.wuyts@student.kdg.be>
2022-11-29 11:49:19 +0100

    Started playerUI

--- risk-front-end/src/components/Game.tsx
+++ risk-front-end/src/components/Game.tsx
@@ -1,12 +1,21 @@
 import Board from "./Board";
+import PlayerFrame from "./PlayerFrame";
+import Grid from "@mui/material/Grid";
 
 export default function Game() {
     const selectCountry = (e: any, country: string) => {
         console.log(e, country);
     }
     return (
-        <div style={{backgroundColor:"lightblue"}}>
-            <Board selectCountry={selectCountry}/>
+        <div style={{backgroundColor:"white"}}>
+            <Grid container>
+                <Grid item xs={10}>
+                    <Board selectCountry={selectCountry}/>
+                </Grid>
+                <Grid item xs={2}>
+                    <PlayerFrame/>
+                </Grid>
+            </Grid>
         </div>
     );
 }
