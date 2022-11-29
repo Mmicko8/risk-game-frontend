a2034a8decbc741c1e21a96860ca0b2e9b0b2ccf
Alexander Wuyts <alexander.wuyts@student.kdg.be>
2022-11-29 19:22:54 +0100

    Started changing register

--- risk-front-end/src/components/identity/Register.tsx
+++ risk-front-end/src/components/identity/Register.tsx
@@ -8,105 +8,107 @@ import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import {createTheme, ThemeProvider} from '@mui/material/styles';
 import {Link} from "react-router-dom";
+import {useForm, Controller} from "react-hook-form";
+import {SignUpCredentials} from "../../model/SignUpCredentials";
 
 function Copyright(props: any) {
     return (
         <Typography variant="body2" color="text.secondary" align="center" {...props}>
-            {'Copyright © '}
-            <Link to="/">
-                Risky Business
-            </Link>{' '}
-            {new Date().getFullYear()}
-            {'.'}
+            Copyright © Risky Business {new Date().getFullYear()}.
         </Typography>
     );
 }
 
-const theme = createTheme();
+const REQUIRED_FIELD_MESSAGE = 'This field is required'
 
 export default function SignUp() {
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        console.log({
-            email: data.get('email'),
-            password: data.get('password'),
-        });
+    const {
+        control,
+        handleSubmit,
+        reset,
+    } = useForm({
+        defaultValues: {
+            name: '',
+            username: '',
+            email: '',
+            password: '',
+        }
+    })
+
+    const _onSubmit = (data: SignUpCredentials) => {
+        reset()
     };
 
     return (
-        <ThemeProvider theme={theme}>
-            <Container component="main" maxWidth="xs">
-                <CssBaseline/>
-                <Box
-                    sx={{
-                        marginTop: 8,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    }}
-                >
-                    <Avatar sx={{m: 1, bgcolor: 'secondary.main'}}>
-                        <LockOutlinedIcon/>
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                        Sign up
-                    </Typography>
-                    <Box component="form" noValidate onSubmit={handleSubmit} sx={{mt: 3}}>
-                        <Grid container spacing={2}>
-                            <Grid item xs={12}>
-                                <TextField
-                                    required
-                                    fullWidth
-                                    id="username"
-                                    label="Username"
-                                    name="username"
-                                    autoComplete="username"
-                                />
-                            </Grid><Grid item xs={12}>
+        <Container component="main" maxWidth="xs">
+            <CssBaseline/>
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <Avatar sx={{m: 1, bgcolor: 'secondary.main'}}>
+                    <LockOutlinedIcon/>
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign up
+                </Typography>
+                <Box component="form" onSubmit={handleSubmit(_onSubmit)} sx={{mt: 3}}>
+                    <Controller
+                        name="name"
+                        control={control}
+                        rules={{required: REQUIRED_FIELD_MESSAGE}}
+                        render={({field}) => (
                             <TextField
+                                {...field}
                                 required
                                 fullWidth
-                                id="email"
-                                label="Email Address"
-                                name="email"
-                                autoComplete="email"
+                                id="username"
+                                label="Username"
+                                autoComplete="username"
                             />
+                        )}
+                    />
+                    <TextField
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                    />
+                    <TextField
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="new-password"
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{mt: 3, mb: 2}}
+                    >
+                        Sign Up
+                    </Button>
+                    <Grid container justifyContent="flex-end">
+                        <Grid item>
+                            <Link to={"/sign_in"}>
+                                Already have an account? Sign in
+                            </Link>
                         </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    required
-                                    fullWidth
-                                    name="password"
-                                    label="Password"
-                                    type="password"
-                                    id="password"
-                                    autoComplete="new-password"
-                                />
-                            </Grid>
-                        </Grid>
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{mt: 3, mb: 2}}
-                        >
-                            Sign Up
-                        </Button>
-                        <Grid container justifyContent="flex-end">
-                            <Grid item>
-                                <Link to={"/sign_in"}>
-                                    Already have an account? Sign in
-                                </Link>
-                            </Grid>
-                        </Grid>
-                    </Box>
+                    </Grid>
                 </Box>
-                <Copyright sx={{mt: 5}}/>
-            </Container>
-        </ThemeProvider>
+            </Box>
+            <Copyright sx={{mt: 5}}/>
+        </Container>
     );
 }
 
