c93f8340ab767f778378977805d77799110163d9
Michel Matthe <michel.matthe@student.kdg.be>
2022-12-13 16:17:42 +0100

    refactored models to receive card data from gameState

--- risk-front-end/src/model/GameModel.ts
+++ risk-front-end/src/model/GameModel.ts
@@ -1,10 +1,12 @@
 import {PlayerInGame} from "./PlayerInGame";
 import {Continent} from "./Continent";
+import {Card} from "./Card";
 
 export interface GameModel {
     gameId: number;
     continents: Continent[];
     playersInGame: PlayerInGame[];
+    cards: Card[];
     turn: number;
     currentPlayerIndex: number;
     phase: string;
