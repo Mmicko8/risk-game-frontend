41a836c22b2257fc1cd551287cf51513c2aeb8aa
Alexander Wuyts <alexander.wuyts@student.kdg.be>
2022-12-13 10:37:37 +0100

    Creating lobby finished

--- risk-front-end/src/components/Home.tsx
+++ risk-front-end/src/components/Home.tsx
@@ -1,13 +1,33 @@
-import {Fab, Tooltip} from "@mui/material"
+import {Fab, Tooltip, CircularProgress, Alert} from "@mui/material"
 import AddIcon from "@mui/icons-material/Add";
 import {useContext, useState} from "react";
 import CreateLobby from "./dialogs/CreateLobby";
 import AccessContext from "../context/AccessContext";
+import {CreateLobbyData, CreateLobbyDataNoUsername} from "../model/CreateLobbyData";
+import {useLobby} from "../hooks/useLobby";
 
 
 export default function Home() {
+    const {createLobbyMutate, isCreatingLobby, isErrorCreatingLobby} = useLobby("")
     const [isCreateLobbyOpen, setIsCreateLobbyOpen] = useState(false);
     const {username} = useContext(AccessContext);
+
+    function createLobby(data: CreateLobbyDataNoUsername) {
+        if (username) {
+            const createLobbyData: CreateLobbyData = {
+                username: username,
+                ...data
+            }
+            createLobbyMutate({...createLobbyData})
+        }
+    }
+
+    if (isCreatingLobby) {
+        return <CircularProgress sx={{position: "fixed", top: "50%", left: "50%"}}/>
+    }
+    if (isErrorCreatingLobby) {
+        return <Alert severity="error">Error creating the lobby</Alert>
+    }
 
     return (
         <>
@@ -17,7 +37,7 @@ export default function Home() {
                         <Tooltip title="Create lobby">
                             <Fab
                                 size="large"
-                                color="secondary"
+                                color="primary"
                                 aria-label="createLobby"
                                 onClick={() => setIsCreateLobbyOpen(true)}
                             >
@@ -27,7 +47,7 @@ export default function Home() {
                     </div>
                 </>}
             <h1>All open lobbies</h1>
-            <CreateLobby isOpen={isCreateLobbyOpen} onClose={() => setIsCreateLobbyOpen(false)}/>
+            <CreateLobby isOpen={isCreateLobbyOpen} onClose={() => setIsCreateLobbyOpen(false)} onSubmit={createLobby}/>
         </>
     );
 }
--- risk-front-end/src/components/dialogs/CreateLobby.tsx
+++ risk-front-end/src/components/dialogs/CreateLobby.tsx
@@ -1,28 +1,73 @@
 import {
     Dialog,
     DialogActions,
-    DialogContent
+    DialogContent,
+    DialogTitle,
+    FormControl,
+    InputLabel,
+    MenuItem,
+    Select,
 } from "@mui/material";
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import {Controller, useForm} from "react-hook-form";
+import {CreateLobbyDataNoUsername} from "../../model/CreateLobbyData";
 
 interface CreateLobbyProps {
     isOpen: boolean;
     onClose: () => void;
+    onSubmit: (data: CreateLobbyDataNoUsername) => void;
 }
 
-export default function CreateLobby({isOpen, onClose} : CreateLobbyProps) {
+export default function CreateLobby({isOpen, onClose, onSubmit}: CreateLobbyProps) {
+    const {
+        control,
+        handleSubmit,
+        reset,
+    } = useForm({
+        defaultValues: {
+            amountOfPlayers: 5
+        }
+    })
+
+    const _onSubmit = (data: CreateLobbyDataNoUsername) => {
+        onSubmit(data);
+        reset();
+        onClose();
+    }
+
     return (
         <Dialog open={isOpen} onClose={onClose} fullWidth maxWidth="xs">
-            <DialogContent>
-                <Box sx={{paddingY: "1rem", paddingX: "4rem"}}>
-                    <Typography id="input-slider" gutterBottom>Create lobby</Typography>
-                </Box>
-            </DialogContent>
-            <DialogActions>
-                <Button variant="contained" fullWidth>Create</Button>
-            </DialogActions>
+            <form onSubmit={handleSubmit(_onSubmit)}>
+                <DialogTitle>Create lobby</DialogTitle>
+                <DialogContent>
+                    <Box sx={{paddingY: "1rem", paddingX: "4rem"}}>
+                        <FormControl fullWidth>
+                            <InputLabel id="amount-of-players-label">Amount of players</InputLabel>
+                            <Controller
+                                name="amountOfPlayers"
+                                control={control}
+                                render={({field}) => (
+                                    <Select
+                                        {...field}
+                                        labelId="amount-of-players-label"
+                                        id="amount-of-players"
+                                        label="Amount of players"
+                                    >
+                                        <MenuItem value={2}>2</MenuItem>
+                                        <MenuItem value={3}>3</MenuItem>
+                                        <MenuItem value={4}>4</MenuItem>
+                                        <MenuItem value={5}>5</MenuItem>
+                                    </Select>
+                                )}
+                            />
+                        </FormControl>
+                    </Box>
+                </DialogContent>
+                <DialogActions>
+                    <Button type="submit" variant="contained" fullWidth>Create</Button>
+                </DialogActions>
+            </form>
         </Dialog>
     );
 }
