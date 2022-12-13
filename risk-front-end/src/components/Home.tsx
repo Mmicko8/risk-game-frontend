07666863fc18a3e95810517a7c24bd8a4152acc5
Alexander Wuyts <alexander.wuyts@student.kdg.be>
2022-12-13 18:01:49 +0100

    Fixed open lobbies + Tryout to not show already joined lobbies in open lobbies

--- risk-front-end/src/components/Home.tsx
+++ risk-front-end/src/components/Home.tsx
@@ -1,36 +1,42 @@
-import {Fab, Tooltip, CircularProgress, Alert} from "@mui/material"
+import {Fab, Tooltip, CircularProgress, Alert, Container} from "@mui/material"
 import AddIcon from "@mui/icons-material/Add";
 import {useContext, useState} from "react";
 import CreateLobby from "./dialogs/CreateLobby";
 import AccessContext from "../context/AccessContext";
 import {CreateLobbyData, CreateLobbyDataNoUsername} from "../model/CreateLobbyData";
+import {createLobbyCall} from "../services/lobbyService";
 import {useLobbies} from "../hooks/useLobbies";
+import {OpenLobbies} from "./OpenLobbies";
+import Typography from "@mui/material/Typography";
+import * as React from "react";
 
 
 export default function Home() {
-    const {createLobbyMutate, isCreatingLobby, isErrorCreatingLobby} = useLobbies()
     const [isCreateLobbyOpen, setIsCreateLobbyOpen] = useState(false);
     const {username} = useContext(AccessContext);
+    //TODO: Request help with this magic => refresh gives all lobbies, then re-renders to correct lobbies that are not joined
+    const {isLoading, isError, lobbies} = useLobbies(20, username);
 
-    function createLobby(data: CreateLobbyDataNoUsername) {
+    async function createLobby(data: CreateLobbyDataNoUsername) {
         if (username) {
             const createLobbyData: CreateLobbyData = {
                 username: username,
                 ...data
             }
-            createLobbyMutate({...createLobbyData})
+            const lobbyId = await createLobbyCall(createLobbyData);
+            console.log(lobbyId);
         }
     }
 
-    if (isCreatingLobby) {
+    if (isLoading) {
         return <CircularProgress sx={{position: "fixed", top: "50%", left: "50%"}}/>
     }
-    if (isErrorCreatingLobby) {
-        return <Alert severity="error">Error creating the lobby</Alert>
+    if (isError) {
+        return <Alert severity="error">Error loading lobbies</Alert>
     }
 
     return (
-        <>
+        <Container maxWidth="lg">
             {!username? "": <>
                     <h1>All joined or created lobbies</h1>
                     <div style={{position: "absolute", right: "20px", bottom: "20px"}}>
@@ -46,8 +52,11 @@ export default function Home() {
                         </Tooltip>
                     </div>
                 </>}
-            <h1>All open lobbies</h1>
+            <Typography component="h1" variant="h4" fontFamily="Courier" fontWeight="bolder">
+                All open lobbies
+            </Typography>
+            <OpenLobbies lobbies={lobbies}/>
             <CreateLobby isOpen={isCreateLobbyOpen} onClose={() => setIsCreateLobbyOpen(false)} onSubmit={createLobby}/>
-        </>
+        </Container>
     );
 }
