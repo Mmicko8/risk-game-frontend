9cded90226b6a0f10006cb7c1d0197b179dbf135
Michel Matthe <michel.matthe@student.kdg.be>
2023-01-06 00:51:56 +0100

    added achievement, item and loyalty point stats to profile

--- risk-front-end/src/components/identity/profile/Profile.tsx
+++ risk-front-end/src/components/identity/profile/Profile.tsx
@@ -60,6 +60,15 @@ export default function Profile() {
 
     // Instellen en bekijken van nickname, avatar, password reset, stats (# spelletjes, #gewonnen…),...
     console.log(profile)
+    console.log(theme.palette)
+
+    const statSx = {color: theme.palette.primary.main, fontSize: "80px"}
+    const stats = [
+        {icon: <AchievementIcon sx={statSx}/>, amount: profile.achievements.length, text: "Achievements"},
+        {icon: <LoyaltyPointsIcon sx={statSx}/>, amount: profile.loyaltyPoints, text: "Loyalty Points"},
+        {icon: <ShopItemIcon sx={statSx}/>, amount: profile.shopItems.length, text: "Owned Items"}
+    ]
+
 
     const statSx = {color: theme.palette.primary.main, fontSize: "80px"}
     const stats = [
