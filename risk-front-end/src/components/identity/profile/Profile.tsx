887a600017908a9d7cfa701ec82d056f04e7bb0d
Michel Matthe <michel.matthe@student.kdg.be>
2023-01-06 00:51:56 +0100

    added achievement, item and loyalty point stats to profile

--- risk-front-end/src/components/identity/profile/Profile.tsx
+++ risk-front-end/src/components/identity/profile/Profile.tsx
@@ -8,10 +8,17 @@ import * as React from "react";
 import EditProfile from "./EditProfile";
 import {useContext} from "react";
 import AccessContext from "../../../context/AccessContext";
+import AchievementIcon from '@mui/icons-material/MilitaryTech';
+import LoyaltyPointsIcon from '@mui/icons-material/MonetizationOn';
+import ShopItemIcon from '@mui/icons-material/ShoppingBag';
+import Typography from "@mui/material/Typography";
+import Divider from '@mui/material/Divider';
+import {useTheme} from "@mui/material/styles";
 
 export default function Profile() {
     const {isLoading, isError, profile} = useProfile();
     const {username} = useContext(AccessContext);
+    const theme = useTheme();
 
     if (isLoading) {
         return <Loading/>
@@ -22,6 +29,15 @@ export default function Profile() {
 
     // Instellen en bekijken van nickname, avatar, password reset, stats (# spelletjes, #gewonnen…),...
     console.log(profile)
+    console.log(theme.palette)
+
+    const statSx = {color: theme.palette.primary.main, fontSize: "80px"}
+    const stats = [
+        {icon: <AchievementIcon sx={statSx}/>, amount: profile.achievements.length, text: "Achievements"},
+        {icon: <LoyaltyPointsIcon sx={statSx}/>, amount: profile.loyaltyPoints, text: "Loyalty Points"},
+        {icon: <ShopItemIcon sx={statSx}/>, amount: profile.shopItems.length, text: "Owned Items"}
+    ]
+
 
     return <Container>
         <Grid container sx={{marginTop: "10vh"}}>
@@ -31,8 +47,15 @@ export default function Profile() {
                     <EditProfile id={profile.id} username={username!} email={profile.email}/>
                 </div>
             </Grid>
-            <Grid item xs={12} md={6}>
-
+            <Grid item xs={12} md={6} sx={{display: "flex", justifyContent: "space-around", alignItems: "center"}}>
+                {stats.map((stat, index) =>
+                    <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+                        {stat.icon}
+                        <Typography variant="h4">{stat.amount}</Typography>
+                        <div style={{color: theme.palette.grey.A700}}>{stat.text}</div>
+                        <Divider orientation="vertical" flexItem/>
+                    </div>
+                )}
             </Grid>
         </Grid>
     </Container>
