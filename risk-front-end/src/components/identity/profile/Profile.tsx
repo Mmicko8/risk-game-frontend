5791a5e874728e7f28b23a0e7669b450982f2cf2
Michel Matthe <michel.matthe@student.kdg.be>
2023-01-05 23:37:23 +0100

    started profile component

---
+++ risk-front-end/src/components/identity/profile/Profile.tsx
@@ -0,0 +1,36 @@
+import {useProfile} from "../../../hooks/useProfile";
+import Loading from "../../Loading";
+import {Alert} from "../../Alert";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Avatar from "@mui/material/Avatar";
+import * as React from "react";
+import EditProfile from "./EditProfile";
+
+export default function Profile() {
+    const {isLoading, isError, profile} = useProfile();
+
+    if (isLoading) {
+        return <Loading/>
+    }
+    if (isError) {
+        return <Alert message={"Error loading the profile"}/>
+    }
+
+    // Instellen en bekijken van nickname, avatar, password reset, stats (# spelletjes, #gewonnen…),...
+    console.log(profile)
+
+    return <Container>
+        <Grid container sx={{marginTop: "10vh"}}>
+            <Grid item xs={12} md={6}>
+                <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+                    <Avatar sx={{width: "120px", height: "120px"}} src="/testAvatar.jpg"/>
+                    <EditProfile id={profile.id} username={profile.username} email={profile.email}/>
+                </div>
+            </Grid>
+            <Grid item xs={12} md={6}>
+
+            </Grid>
+        </Grid>
+    </Container>
+}
