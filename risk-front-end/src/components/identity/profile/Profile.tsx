db0f25ea7c9be5fd76b7dd7cc772720ef87e95ec
Michel Matthe <michel.matthe@student.kdg.be>
2023-01-06 02:15:05 +0100

    added game stats to Profile.tsx

--- risk-front-end/src/components/identity/profile/Profile.tsx
+++ risk-front-end/src/components/identity/profile/Profile.tsx
@@ -12,8 +12,8 @@ import AchievementIcon from '@mui/icons-material/MilitaryTech';
 import LoyaltyPointsIcon from '@mui/icons-material/MonetizationOn';
 import ShopItemIcon from '@mui/icons-material/ShoppingBag';
 import Typography from "@mui/material/Typography";
-import Divider from '@mui/material/Divider';
 import {useTheme} from "@mui/material/styles";
+import Divider from "@mui/material/Divider"
 
 export default function Profile() {
     const {isLoading, isError, profile} = useProfile();
@@ -29,7 +29,6 @@ export default function Profile() {
 
     // Instellen en bekijken van nickname, avatar, password reset, stats (# spelletjes, #gewonnen…),...
     console.log(profile)
-    console.log(theme.palette)
 
     const statSx = {color: theme.palette.primary.main, fontSize: "80px"}
     const stats = [
@@ -47,15 +46,42 @@ export default function Profile() {
                     <EditProfile id={profile.id} username={username!} email={profile.email}/>
                 </div>
             </Grid>
-            <Grid item xs={12} md={6} sx={{display: "flex", justifyContent: "space-around", alignItems: "center"}}>
-                {stats.map((stat, index) =>
-                    <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
-                        {stat.icon}
-                        <Typography variant="h4">{stat.amount}</Typography>
-                        <div style={{color: theme.palette.grey.A700}}>{stat.text}</div>
-                        <Divider orientation="vertical" flexItem/>
+            <Grid item xs={12} md={6} style={{display: "flex", flexDirection: "column", justifyContent: "center", gap: "50px"}}>
+                <div>
+                    <Typography variant="h4"  sx={{display:"flex", justifyContent:"center", marginBottom: "20px"}}>
+                        Trophies
+                    </Typography>
+                    <div style={{display: "flex", justifyContent: "space-around", alignItems: "center"}}>
+                        {stats.map((stat, index) =>
+                            <div key={index} style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+                                {stat.icon}
+                                <Typography variant="h4">{stat.amount}</Typography>
+                                <div style={{color: theme.palette.grey.A700}}>{stat.text}</div>
+                            </div>
+                        )}
                     </div>
-                )}
+                </div>
+                <div>
+                    <Typography variant="h4" sx={{display:"flex", justifyContent:"center", marginBottom: "20px"}}>
+                        Games
+                    </Typography>
+                    <div style={{display: "flex", justifyContent: "space-around", alignItems: "center"}}>
+                        <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+                            <Typography variant="h4">{profile.gamesPlayed}</Typography>
+                            <div style={{color: theme.palette.grey.A700}}>PLAYED</div>
+                        </div>
+                        <Divider orientation="vertical" flexItem/>
+                        <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+                            <Typography variant="h4">{profile.gamesWon}</Typography>
+                            <div style={{color: theme.palette.grey.A700}}>WON</div>
+                        </div>
+                        <Divider orientation="vertical" flexItem/>
+                        <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+                            <Typography variant="h4">{profile.gamesLost}</Typography>
+                            <div style={{color: theme.palette.grey.A700}}>LOST</div>
+                        </div>
+                    </div>
+                </div>
             </Grid>
         </Grid>
     </Container>
