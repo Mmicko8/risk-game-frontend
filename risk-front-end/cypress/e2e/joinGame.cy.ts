00e31887acf23a4db20948ce58c2eb8d6d0f7b42
Alexander Wuyts <alexander.wuyts@student.kdg.be>
2023-01-09 00:16:20 +0100

    Updated cypress test with before all and changed cypress to dev-dependency

--- risk-front-end/cypress/e2e/joinGame.cy.ts
+++ risk-front-end/cypress/e2e/joinGame.cy.ts
@@ -1,6 +1,14 @@
 /// <reference types="cypress" />
 
 // @ts-ignore
+before(() => {
+    cy.visit('http://localhost:3000/sign_in')
+    cy.get('#username').type('KdgUser1').should('have.value', 'KdgUser1')
+    cy.get('#password').type('password').should('have.value', 'password')
+    cy.contains('Sign In').click()
+    cy.url().should('equal', 'http://localhost:3000/')
+})
+
 describe('Joining a game (assuming the backend runs and has correct test data)', () => {
     it('Should show the game', () => {
         // cy.get('[data-testid=LogoutIcon]').click() //-> logout?
